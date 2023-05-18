2b844fe3ada9e4e3b001f09bf1bf5d5ad78d761b
Mikhail Timofeev <mtimofeev@matmgt.com>
2023-05-18 16:34:51 +0400

    update registerDomain

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,14 +14,14 @@ fun main(args: Array<String>): Unit = runBlocking{
     val adminKeyPair = keyPairFromHex("7233bfc89dcbd68c19fde6ce6158225298ec1131b6a130d1aeb454c1ab5183c0",
                                       "9ac47abf59b356e0bd7dcbbbb4dec080e302156a48ca907e47cb6aea1d32719e")
 
-    val query = Query(peerUrl, telemetryUrl, admin, adminKeyPair)
+    val sendTransaction = SendTransaction(peerUrl, telemetryUrl, admin, adminKeyPair)
 
     val domain = "domain_${System.currentTimeMillis()}"
-    query.registerDomain(domain).also { println("DOMAIN $domain CREATED") }
+    sendTransaction.registerDomain(domain).also { println("DOMAIN $domain CREATED") }
 
 }
 
-open class Query (peerUrl: String,
+open class SendTransaction (peerUrl: String,
                     telemetryUrl: String,
                     private val admin: AccountId,
                     private val keyPair: KeyPair,
