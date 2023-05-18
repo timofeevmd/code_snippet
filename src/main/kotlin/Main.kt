f87266a81c2d74d72bb1a2ddea3d7e1721ba1b26
Mikhail Timofeev <mtimofeev@matmgt.com>
2023-05-18 16:40:04 +0400

    registerAccount

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 import jp.co.soramitsu.iroha2.*
+import jp.co.soramitsu.iroha2.generated.crypto.PublicKey
 import jp.co.soramitsu.iroha2.generated.datamodel.Value
 import jp.co.soramitsu.iroha2.generated.datamodel.account.AccountId
+import jp.co.soramitsu.iroha2.generated.datamodel.metadata.Metadata
 import jp.co.soramitsu.iroha2.generated.datamodel.name.Name
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeout
@@ -18,6 +20,11 @@ fun main(args: Array<String>): Unit = runBlocking{
 
     val domain = "domain_${System.currentTimeMillis()}"
     sendTransaction.registerDomain(domain).also { println("DOMAIN $domain CREATED") }
+
+    val joe = "joe_${System.currentTimeMillis()}$ACCOUNT_ID_DELIMITER$domain"
+    val joeKeyPair = generateKeyPair()
+    sendTransaction.registerAccount(joe, listOf(joeKeyPair.public.toIrohaPublicKey()))
+        .also { println("ACCOUNT $joe CREATED") }
 
 }
 
@@ -38,6 +45,22 @@ open class SendTransaction (peerUrl: String,
         client.sendTransaction {
             account(admin)
             this.registerDomain(id.asDomainId(), metadata)
+            buildSigned(keyPair)
+        }.also {
+            withTimeout(timeout) { it.await() }
+        }
+    }
+
+    suspend fun registerAccount(
+        id: String,
+        signatories: List<PublicKey>,
+        metadata: Map<Name, Value> = mapOf(),
+        admin: AccountId = this.admin,
+        keyPair: KeyPair = this.keyPair
+    ) {
+        client.sendTransaction {
+            account(admin)
+            this.registerAccount(id.asAccountId(), signatories, Metadata(metadata))
             buildSigned(keyPair)
         }.also {
             withTimeout(timeout) { it.await() }
